00decc82bdbd54bc27f1801eefaf5a50611fb6b7
pperezp <patodeath@gmail.com>
2020-07-19 01:28:47 -0400

    Now is a maven project

---
+++ src/main/java/gnu/sql/tools/sqladmin/App.java
@@ -0,0 +1,13 @@
+package gnu.sql.tools.sqladmin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
